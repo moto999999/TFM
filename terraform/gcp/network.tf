dfbc55e660ce9713df5fa541a9098fc3ca9c20c1
Juan Ramírez <juan1999l@gmail.com>
2022-10-26 13:24:39 +0200

    terraform: gcp: Add router nat
      * Needed for having internet connection without exposing machines to the internet

--- terraform/gcp/network.tf
+++ terraform/gcp/network.tf
@@ -7,4 +7,28 @@ resource "google_compute_subnetwork" "nodes_subnet" {
   name          = var.subnetwork_name
   ip_cidr_range = var.ip_cidr_range
   network       = google_compute_network.k8s_network.id
+  region        = var.region
+}
+
+resource "google_compute_router" "router" {
+  name    = "router-k8s"
+  region  = google_compute_subnetwork.nodes_subnet.region
+  network = google_compute_network.k8s_network.id
+
+  bgp {
+    asn = 64514
+  }
+}
+
+resource "google_compute_router_nat" "nat" {
+  name                               = "router-k8s-nat"
+  router                             = google_compute_router.router.name
+  region                             = var.region
+  nat_ip_allocate_option             = "AUTO_ONLY"
+  source_subnetwork_ip_ranges_to_nat = "ALL_SUBNETWORKS_ALL_IP_RANGES"
+
+  log_config {
+    enable = true
+    filter = "ERRORS_ONLY"
+  }
 }
