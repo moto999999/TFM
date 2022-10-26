5630cc15f79118e6d07020f1f74efad16bea8042
Juan Ramírez <juan1999l@gmail.com>
2022-10-26 15:23:31 +0200

    terraform: gcp: Modify ssh firewall rule
      * Only allow external ssh connections to bastion
      * Allow ssh connections from bastion to control plane and workers

--- terraform/gcp/firewall.tf
+++ terraform/gcp/firewall.tf
@@ -9,6 +9,21 @@ resource "google_compute_firewall" "firewall_rule_bastion_ssh" {
   }
 
   source_ranges = ["0.0.0.0/0"] # It would be better if only known hosts were allowed
+  target_tags   = [var.bastion_tag]
+}
+
+resource "google_compute_firewall" "firewall_rule_internal_ssh" {
+  name        = "ssh-internal"
+  network     = google_compute_network.k8s_network.id
+  description = "Create firewall rule to allow internal ssh connections from bastion"
+
+  allow {
+    protocol = "tcp"
+    ports    = ["22"]
+  }
+
+  source_tags = [var.bastion_tag]
+  target_tags = [var.control_plane_tag, var.worker_tag]
 }
 
 resource "google_compute_firewall" "firewall_rule_bastion_inside" {
