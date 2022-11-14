06bcc77e9fc7441588e22099f22cb951956f2879
Juan Ramírez <juan1999l@gmail.com>
2022-11-14 19:32:45 +0100

    utilities: install ansible.posix in bastion when copying role

--- utilities/copy-ansible-role-to-bastion.sh
+++ utilities/copy-ansible-role-to-bastion.sh
@@ -1,3 +1,4 @@
 bastion=`cd ../terraform/gcp && terraform output | tr -d '"' | grep "bastion:" | sed 's/^.*: //'`
 
 scp -r -i ../ssh_key/admin ../deploy-k8s admin@$bastion:~/
+ssh -i ../ssh_key/admin admin@$bastion ansible-galaxy collection install ansible.posix
