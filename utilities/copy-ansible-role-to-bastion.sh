518775f37962e3c23077a12b8065337b11ce2787
Juan Ramírez <juan1999l@gmail.com>
2022-11-14 19:32:23 +0100

    utilities: get bastion IP from terraform output

--- utilities/copy-ansible-role-to-bastion.sh
+++ utilities/copy-ansible-role-to-bastion.sh
@@ -1,7 +1,3 @@
-#! /bin/bash
-if [ $# -eq 0 ]; then
-    echo "This script requires 1 argument: ./copy-ansible-role-to-bastion.sh [bastion_ip]"
-    exit 1
-fi
+bastion=`cd ../terraform/gcp && terraform output | tr -d '"' | grep "bastion:" | sed 's/^.*: //'`
 
-scp -r -i ../ssh_key/admin ../deploy-k8s admin@$1:~/
+scp -r -i ../ssh_key/admin ../deploy-k8s admin@$bastion:~/
--- utilities/copy-ssh-key-to-bastion.sh
+++ utilities/copy-ssh-key-to-bastion.sh
@@ -1,8 +1,4 @@
-#! /bin/bash
-if [ $# -eq 0 ]; then
-    echo "This script requires 1 argument: ./copy-ssh-key-to-bastion.sh [bastion_ip]"
-    exit 1
-fi
+bastion=`cd ../terraform/gcp && terraform output | tr -d '"' | grep "bastion:" | sed 's/^.*: //'`
 
-scp -i ../ssh_key/admin ../ssh_key/admin admin@$1:~/.ssh
-scp -i ../ssh_key/admin ../ssh_key/admin.pub admin@$1:~/.ssh
+scp -i ../ssh_key/admin ../ssh_key/admin admin@$bastion:~/.ssh
+scp -i ../ssh_key/admin ../ssh_key/admin.pub admin@$bastion:~/.ssh
