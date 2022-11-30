227b67622e388ebe13b0b1a7ab65da84748926b8
Juan Ramírez <juan1999l@gmail.com>
2022-11-30 16:58:32 +0100

    terraform: gcp: Allow internal communication across TCP, UDP, ICMP and IP in IP
      * As it's only between local machines, allow all internal communications

--- terraform/gcp/firewall.tf
+++ terraform/gcp/firewall.tf
@@ -26,19 +26,6 @@ resource "google_compute_firewall" "firewall_rule_internal_ssh" {
   target_tags = [var.control_plane_tag, var.worker_tag]
 }
 
-resource "google_compute_firewall" "firewall_rule_bastion_inside" {
-  name        = "ssh-inside-network-bastion"
-  network     = google_compute_network.k8s_network.id
-  description = "Create firewall rule to allow ssh connections from bastion to rest of the cluster"
-
-  allow {
-    protocol = "tcp"
-    ports    = ["22"]
-  }
-  source_tags = [var.bastion_tag]
-  target_tags = [var.control_plane_tag, var.worker_tag]
-}
-
 resource "google_compute_firewall" "firewall_rule_api_server" {
   name        = "kubernetes-api-server"
   network     = google_compute_network.k8s_network.id
@@ -52,84 +39,25 @@ resource "google_compute_firewall" "firewall_rule_api_server" {
   target_tags   = [var.control_plane_tag]
 }
 
-resource "google_compute_firewall" "firewall_rule_etcd" {
-  name        = "etcd-server-client-api"
+resource "google_compute_firewall" "firewall_rule_all_all_internal" {
+  name        = "allow-all-internal"
   network     = google_compute_network.k8s_network.id
-  description = "Creates firewall rule for etcd server"
+  description = "Creates firewall rule for Typha"
 
   allow {
     protocol = "tcp"
-    ports    = ["2379-2380"]
   }
 
-  source_tags = [var.control_plane_tag, var.worker_tag]
-  target_tags = [var.control_plane_tag]
-}
-
-resource "google_compute_firewall" "firewall_rule_kubelet_api" {
-  name        = "kubelet-api"
-  network     = google_compute_network.k8s_network.id
-  description = "Creates firewall rule for kubelet api"
-
   allow {
-    protocol = "tcp"
-    ports    = ["10250"]
+    protocol = "udp"
   }
 
-  source_tags = [var.control_plane_tag, var.worker_tag]
-  target_tags = [var.control_plane_tag, var.worker_tag]
-}
-
-resource "google_compute_firewall" "firewall_rule_kube_scheduler" {
-  name        = "kube-scheduler"
-  network     = google_compute_network.k8s_network.id
-  description = "Creates firewall rule for kube scheduler"
-
   allow {
-    protocol = "tcp"
-    ports    = ["10259"]
+    protocol = "icmp"
   }
 
-  source_tags = [var.control_plane_tag]
-  target_tags = [var.control_plane_tag]
-}
-
-resource "google_compute_firewall" "firewall_rule_kube_controller_manager" {
-  name        = "kube-controller-manager"
-  network     = google_compute_network.k8s_network.id
-  description = "Creates firewall rule for kube controller manager"
-
   allow {
-    protocol = "tcp"
-    ports    = ["10257"]
-  }
-
-  source_tags = [var.control_plane_tag]
-  target_tags = [var.control_plane_tag]
-}
-
-resource "google_compute_firewall" "firewall_rule_nodeport_services" {
-  name        = "nodeport-services"
-  network     = google_compute_network.k8s_network.id
-  description = "Creates firewall rule for NodePort Services"
-
-  allow {
-    protocol = "tcp"
-    ports    = ["30000-32767"]
-  }
-
-  source_tags = [var.worker_tag]
-  target_tags = [var.control_plane_tag, var.worker_tag]
-}
-
-resource "google_compute_firewall" "firewall_rule_bgp" {
-  name        = "bgp"
-  network     = google_compute_network.k8s_network.id
-  description = "Creates firewall rule for BGP protocol"
-
-  allow {
-    protocol = "tcp"
-    ports    = ["179"]
+    protocol = "ipip"
   }
 
   source_tags = [var.control_plane_tag, var.worker_tag]
