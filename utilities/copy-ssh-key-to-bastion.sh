16fd18a9743d61d8472991a74fe5cc34dfee46f2
Juan Ramírez <juan1999l@gmail.com>
2022-12-01 17:18:51 +0100

    utilities: Update for ansible inventory refactor
      * Change create-cluster.sh for working on local machine

--- utilities/copy-ssh-key-to-bastion.sh
+++ utilities/copy-ssh-key-to-bastion.sh
@@ -1,4 +1,4 @@
-bastion=`cd ../terraform/gcp && terraform output | tr -d '"' | grep "bastion:" | sed 's/^.*: //'`
+bastion=`cd ../terraform/gcp && terraform output | tr -d '"' | grep "bastion_public_ip:" | sed 's/^.*: //'`
 
 scp -i ../ssh_key/admin ../ssh_key/admin admin@$bastion:~/.ssh
 scp -i ../ssh_key/admin ../ssh_key/admin.pub admin@$bastion:~/.ssh
--- utilities/create-cluster.sh
+++ utilities/create-cluster.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
-cd ../terraform/gcp
-terraform apply -auto-approve
+# Terraform apply
+(cd ../terraform/gcp && terraform apply -auto-approve)
 
-cd ../../utilities
-
-echo "Waiting 25 seconds to wait until ansible is installed in bastion"
-sleep 25
-
+# Set ansible variables and inventory from terraform output
 ./set-ansible-vars-and-inventory.sh
-./copy-ssh-key-to-bastion.sh
-./copy-ansible-role-to-bastion.sh
 
-bastion=`cd ../terraform/gcp && terraform output | tr -d '"' | grep "bastion:" | sed 's/^.*: //'`
-ssh -i ../ssh_key/admin admin@$bastion "cd deploy-k8s/ && ansible-playbook deploy-cluster.yaml -i inventory"
+# Copy SSH key to bastion
+./copy-ssh-key-to-bastion.sh
+
+# Install ansible.posix on local machine
+ansible-galaxy collection install ansible.posix
+
+# Deploy cluster
+(cd ../deploy-k8s/ && ansible-playbook deploy-cluster.yaml -i inventory)
